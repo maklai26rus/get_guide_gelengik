24490c7f664d9ae79013126d741f32599688bf29
Maklai <maklai26rus@mail.ru>
2021-12-17 15:53:49 +0300

    работа над ошибками

--- main_as.py
+++ main_as.py
@@ -10,6 +10,8 @@ headers = {
                   '(KHTML, like Gecko) Chrome/81.0.4044.96 YaBrowser/20.4.0.1461 Yowser/2.5 Safari/537.36',
     'accept': '*/*'}
 
+FINAL_URL = []
+
 
 # def get_url(url):
 #     """Получаем все страницы с категориями"""
@@ -20,12 +22,27 @@ headers = {
 #     url_category = [URL + i for i in url_page_href]
 #     return url_category
 
+async def gather_data():
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(URL, headers=headers)
+
+        soup = BeautifulSoup(await response.text(), "lxml")
+        pages_count = soup.find_all("h3", class_="category-widget__title")
+        url_page_href = [fi.find("a").get('href') for fi in pages_count]
+        url_category = [URL + i for i in url_page_href]
+        tasks = []
+        for page in url_category:
+            task = asyncio.create_task(get_page_data(session, page))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+
 
 async def get_page_data(session, page):
     """Перебираем выбраную категорию и получаем каждый элемент в выбраной категории
     https://gelend.spravker.ru/avto.htm
     получаем
     'https://gelend.spravker.ru/avtoaksessuary/"""
+
     async with session.get(url=page, headers=headers) as response:
         url_page_href = []
         url_category_data = []
@@ -39,11 +56,10 @@ async def get_page_data(session, page):
                 url_page_href.append(_url_cat)
             except AttributeError:
                 continue
-
         url_category_data = [URL + i for i in url_page_href]
         for _page in url_category_data:
             _p = get_max_page(_page)
-            save_cvc_page(_p)
+            await save_cvc_page(_p)
 
         # return url_category_data
 
@@ -64,34 +80,25 @@ def get_max_page(url):
     return list_url_page
 
 
-def save_cvc_page(url):
+async def save_cvc_page(url):
     """получаем данные из каждой категории """
+    _d = None
     for i in url:
+        with open(f'save_url.txt', 'a', encoding='utf-8') as ff:
+            ff.write(i + "\n")
         response = requests.get(i)
-        soup = BeautifulSoup(response.text, "lxml")
-        _name_org = soup.find_all("a", class_="org-widget-header__title-link")
-        _phone = soup.find_all("div", class_="org-widget__spec")
         try:
+            soup = BeautifulSoup(response.text, "lxml")
+            _name_org = soup.find_all("a", class_="org-widget-header__title-link")
+            _phone = soup.find_all("div", class_="org-widget__spec")
             for _data in range(len(_name_org)):
+                _d = str(_name_org[_data].text.strip())
                 with open(f'BD.cvc', 'a', encoding='utf-8') as ff:
                     ff.write(str(_name_org[_data].text.strip() + ',' + _phone[_data].dd.text.strip() + "\n"))
-        except AttributeError:
-            pass
-
-async def gather_data():
-    async with aiohttp.ClientSession() as session:
-        response = await session.get(URL, headers=headers)
-
-        soup = BeautifulSoup(await response.text(), "lxml")
-        pages_count = soup.find_all("h3", class_="category-widget__title")
-        url_page_href = [fi.find("a").get('href') for fi in pages_count]
-        url_category = [URL + i for i in url_page_href]
-        tasks = []
-        # task = asynciocreate_task([URL + i for i in url_page_href])
-        for page in url_category:
-            task = asyncio.create_task(get_page_data(session, page))
-            tasks.append(task)
-        await asyncio.gather(*tasks)
+        except AttributeError as err:
+            with open(f'BD.cvc', 'a', encoding='utf-8') as ff:
+                ff.write(_d + f' нету номера {err} \n')
+            continue
 
 
 def main():
@@ -103,3 +110,27 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# def save_cvc_page1():
+#     """получаем данные из каждой категории """
+#     # for i in url:
+#     kki = ['https://gelend.spravker.ru/shtrafstoianki/page-1/', 'https://gelend.spravker.ru/avtosalony/']
+#     _d = None
+#     for i in kki:
+#         response = requests.get(i)
+#         try:
+#             soup = BeautifulSoup(response.text, "lxml")
+#             _name_org = soup.find_all("a", class_="org-widget-header__title-link")
+#             _phone = soup.find_all("div", class_="org-widget__spec")
+#             for _data in range(len(_name_org)):
+#                 _d = str(_name_org[_data].text.strip())
+#                 with open(f'BD.cvc', 'a', encoding='utf-8') as ff:
+#                     # print(_phone[_data].dd.text.strip())
+#                     ff.write(str(_name_org[_data].text.strip() + ',' + _phone[_data].dd.text.strip() + "\n"))
+#         except AttributeError as err:
+#             with open(f'BD.cvc', 'a', encoding='utf-8') as ff:
+#                 ff.write(_d + ' нету номера \n')
+#             continue
+#
+#
+# save_cvc_page1()
